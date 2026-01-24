66cf0ae2561093edfe42c491db6cd8e9c9f8e070
SBowling4 <bowlisam001@icloud.com>
2026-01-24 14:19:50 -0600

    initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,91 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+	private Command m_autonomousCommand;
+
+	private final RobotContainer m_robotContainer;
+
+	public Robot() {
+		m_robotContainer = new RobotContainer();
+	}
+
+	@Override
+	public void robotPeriodic() {
+		CommandScheduler.getInstance().run();
+	}
+
+	@Override
+	public void disabledInit() {
+		RobotContainer.getFrontVision().setIMUMode(1);
+		RobotContainer.getFrontVision().setIMUMode(1);
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		RobotContainer.getFrontVision().setRobotOrientation();
+		RobotContainer.getRearVision().setRobotOrientation();
+	}
+
+	@Override
+	public void disabledExit() {
+		RobotContainer.getFrontVision().setIMUMode(4);
+		RobotContainer.getRearVision().setIMUMode(4);
+	}
+
+	@Override
+	public void autonomousInit() {
+		m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+		if (m_autonomousCommand != null) {
+			CommandScheduler.getInstance().schedule(m_autonomousCommand);
+		}
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+	}
+
+	@Override
+	public void autonomousExit() {
+	}
+
+	@Override
+	public void teleopInit() {
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.cancel();
+		}
+	}
+
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	@Override
+	public void teleopExit() {
+	}
+
+	@Override
+	public void testInit() {
+		CommandScheduler.getInstance().cancelAll();
+	}
+
+	@Override
+	public void testPeriodic() {
+	}
+
+	@Override
+	public void testExit() {
+	}
+
+	@Override
+	public void simulationPeriodic() {
+	}
+}
---
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.vision;
+
+public class VisionConstants {
+	public static final String FRONT_LIMELIGHT_NAME = "front_ll";
+	public static final String REAR_LIMELIGHT_NAME = "rear_ll";
+}
